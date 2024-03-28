4de7127b1ee8a0b3cbd5f2116f9e1bde72c13b4f
Allan <allan.r.aguiar@aluno.senai.br>
2024-03-27 21:56:08 -0300

    Feat:Corrigindo

--- Frontend/Model/Request/CargaTranspote.cs
+++ Frontend/Model/Request/CargaTranspote.cs
@@ -10,8 +10,6 @@ namespace Frontend.Model.Request
         [StringLength(100, MinimumLength = 10,
             ErrorMessage = "Nome deve ter no minímo 10 caracteres")]
         public string Nome { get; set; }
-        [Required(ErrorMessage = "Data de nascimento é obrigatório")]
-        public DateTime Nascimento { get; set; }
         public string Distancia { get; set; }
         [Required(ErrorMessage = "Insira uma distancia")]
         public string Peso { get; set; }
