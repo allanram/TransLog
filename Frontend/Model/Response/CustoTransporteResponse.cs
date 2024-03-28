e37dd4a37ed87c7c2dd07101537ddb1b14bedb34
Allan <allan.r.aguiar@aluno.senai.br>
2024-03-27 22:02:41 -0300

    Feat: add response

---
+++ Frontend/Model/Response/CustoTransporteResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Frontend.Model.Response
+{
+    public class CustoTransporteResponse
+    {
+        public double Distancia { get; set; }
+
+        public double Peso { get; set; }
+
+        public double largura { get; set; }
+
+        public double Altura { get; set; }
+
+        public double ValorDotransporte { get; set; }
+    }
+}
